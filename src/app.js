dca8b8547a7ea2f32cd94b60fd7b54906dd0bafb
Shivam Sharma <shivam231198@gmail.com>
2020-12-31 13:17:10 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const jwt = require('jsonwebtoken');
+
+var app = express();
+
+// Adding necessary Headers to handle Client side requests
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs');
+});
+
+
+let port = process.env.PORT || 2000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
