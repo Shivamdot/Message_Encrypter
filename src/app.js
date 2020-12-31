4c6ceffccdda6308240a87651c6de81edf2fefd9
Shivam Sharma <shivam231198@gmail.com>
2020-12-31 15:58:05 +0530

    added mysql database serverconnection and messages table creation logic

--- src/app.js
+++ src/app.js
@@ -3,8 +3,44 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
 const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
 
-var app = express();
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "learning"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("MySQL Connected!");
+
+  let checkTable = 'show tables like "messages"';
+
+  con.query(checkTable, function (err, result) {
+    if(err) {
+      console.log("Not able to able to check available tables");
+      return;
+    }
+
+    if(result.length == 0) {
+      let createTable = "CREATE TABLE messages (MsgID VARCHAR(255) PRIMARY KEY, Message VARCHAR(255))";
+  
+      con.query(createTable, function (err, result) {
+        if(err) {
+          console.log("Not able to create meassages table");
+          return;
+        }
+        console.log("messages Table created");
+      });  
+    }
+  });
+  
+  
+});
+
+let app = express();
 
 // Adding necessary Headers to handle Client side requests
 app.use(cors());
